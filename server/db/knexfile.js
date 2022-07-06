3eeaa9dc2e6c24fee1bd32e02a85f33efe6cb616
sarah-emerson <70471216+sarah-emerson@users.noreply.github.com>
2022-07-06 20:41:37 +1200

    Run Gerard's format script, i.e. prettier

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -4,23 +4,23 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: path.join(__dirname, 'dev.sqlite3')
+      filename: path.join(__dirname, 'dev.sqlite3'),
     },
-    useNullAsDefault: true
+    useNullAsDefault: true,
   },
 
   test: {
     client: 'sqlite3',
     connection: {
-      filename: ':memory:'
+      filename: ':memory:',
     },
     migrations: {
-      directory: path.join(__dirname, 'migrations')
+      directory: path.join(__dirname, 'migrations'),
     },
     seeds: {
-      directory: path.join(__dirname, 'seeds')
+      directory: path.join(__dirname, 'seeds'),
     },
-    useNullAsDefault: true
+    useNullAsDefault: true,
   },
 
   production: {
@@ -28,10 +28,10 @@ module.exports = {
     connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
+      tableName: 'knex_migrations',
+    },
+  },
 }
