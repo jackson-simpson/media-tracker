bf95e120146aac8ead8b005d8d7d76e05df27382
Kelly Keating <keating.kel@gmail.com>
2022-11-10 17:09:16 +1300

    knexfiles enforce foreign keys

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -3,24 +3,30 @@ const path = require('path')
 module.exports = {
   development: {
     client: 'sqlite3',
+    useNullAsDefault: true,
     connection: {
       filename: path.join(__dirname, 'dev.sqlite3'),
     },
-    useNullAsDefault: true,
+    pool: {
+      afterCreate: (conn, cb) => conn.run('PRAGMA foreign_keys = ON', cb),
+    },
   },
 
   test: {
     client: 'sqlite3',
+    useNullAsDefault: true,
     connection: {
       filename: ':memory:',
     },
     migrations: {
-      directory: path.join(__dirname, 'migrations'),
+      directory: path.join(__dirname, 'migrations')
     },
     seeds: {
-      directory: path.join(__dirname, 'seeds'),
+      directory: path.join(__dirname, 'seeds')
     },
-    useNullAsDefault: true,
+    pool: {
+      afterCreate: (conn, cb) => conn.run('PRAGMA foreign_keys = ON', cb),
+    },
   },
 
   production: {
