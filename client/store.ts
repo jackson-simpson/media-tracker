6494324bba192580485c932772dc625ad0e95295
sophia-lawler <sophialawler@gmail.com>
2022-12-02 10:02:25 +1300

    update dev tool enhancers in store

--- client/store.ts
+++ client/store.ts
@@ -1,13 +1,10 @@
-import { createStore, applyMiddleware, compose } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from '@redux-devtools/extension'
 import thunk from 'redux-thunk'
 
 import reducers from './reducers'
 
-const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-const store = createStore(
-  reducers,
-  composeEnhancers(applyMiddleware(thunk))
-)
+const store = createStore(reducers, composeWithDevTools(applyMiddleware(thunk)))
 
 export default store
 
