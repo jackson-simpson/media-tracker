e4c637f9a99ed221bb95f42f06c481b0ed73257e
David Jackson Simpson <128451094+jackson-simpson@users.noreply.github.com>
2023-06-14 11:50:45 +1200

    Added first database migration

---
+++ server/db/migrations/20230613234650_Book-table.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  knex.schema.createTable('books', (table) => {
+    table.increments('id').primary()
+    table.string('title')
+    table.string('author')
+  })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable('books')
+}
